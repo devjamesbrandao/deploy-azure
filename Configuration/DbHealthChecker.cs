d2a10d573028c86232d0e8361a9a62d3d580fcd9
James Noleto <muricijames@gmail.com>
2022-08-09 21:42:33 -0300

    feat: health check para verificar conexão com o banco de dados

---
+++ Configuration/DbHealthChecker.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeuTodo.Configuration
+{
+    public class DbHealthChecker
+    {
+        public static async Task TestConnection(DbContext context)
+        {
+            var maxAttemps = 10;
+
+            var delay = 5000;
+
+            for (int i = 0; i < maxAttemps; i++)
+            {
+                var canConnect = CanConnect(context);
+
+                if (canConnect) return;
+
+                await Task.Delay(delay);
+            }
+
+            throw new Exception("Error wating database. Check ConnectionString and ensure database exist");
+        }
+
+        private static bool CanConnect(DbContext context)
+        {
+            try
+            {
+                context.Database.GetAppliedMigrations();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
