b1fb77515234c7d1e4431818a9b42859ef0d6982
James Noleto <muricijames@gmail.com>
2022-08-10 21:18:36 -0300

    refactor: alteração da descrição que aparece no swagger acerca da aplicação

--- Startup.cs
+++ Startup.cs
@@ -57,7 +57,7 @@ namespace MeuTodo
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SQL Lite", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Primeiro Deploy no Azure - API de tarefas com EF e SQL Lite", Version = "v1" });
 
                 c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
                 {
