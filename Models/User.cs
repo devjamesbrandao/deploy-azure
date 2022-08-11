798bebf343294b884a04fc50abcdc5d1b327d9be
James Noleto <muricijames@gmail.com>
2022-08-10 21:09:08 -0300

    feat: entidade para login dos usuário

---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+namespace MeuTodo.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
