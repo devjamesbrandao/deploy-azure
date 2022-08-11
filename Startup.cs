ed9684160c15f5cf7dc4b9c41c15ff9c54958b65
James Noleto <muricijames@gmail.com>
2022-08-10 21:03:34 -0300

    feat: configuração de autenticação por meio JWT Bearer token e injeção de dependências das interfaces

--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,21 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using System.Text;
 using MeuTodo.Configuration;
 using MeuTodo.Data;
+using MeuTodo.Repositorio.Interfaces;
+using MeuTodo.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using static MeuTodo.Configuration.ErrorHandlerMiddleware;
 
 namespace MeuTodo
 {
@@ -24,13 +31,62 @@ namespace MeuTodo
 
             Configuration = builder.Build();
 
+            Settings.Key = Encoding.ASCII.GetBytes(Configuration.GetValue<string>("KeyToken"));
+
             services.AddControllers();
 
             services.AddMvc();
             
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Settings.Key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "SQL Lite", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme.
+                                Enter 'Bearer' [space] and then insert your token in the text input below.
+                                Example: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                                Scheme = "oauth2",
+                                Name = "Bearer",
+                                In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                    }
+                });
 
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 
@@ -39,15 +95,27 @@ namespace MeuTodo
                 c.IncludeXmlComments(xmlPath);
             });
 
+            services.AddTransient<GlobalExceptionHandlerMiddleware>();
+
             services.AddDbContext<AppDataContext>();
+
+            services.AddScoped<IRepository, UserRepository>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())app.UseDeveloperExceptionPage();
 
-            // Garantindo que as migrations serão aplicadas no banco de dados
+            app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
+
+            // Ensuring migrations are applied to the database
             DbMigrationHelpers.EnsureSeedData(app).Wait();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
 
             app.UseSwagger();
                 
@@ -56,8 +124,6 @@ namespace MeuTodo
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
                 c.RoutePrefix = string.Empty;
             });
-
-            app.UseRouting();
 
             app.UseEndpoints(endpoints =>{ endpoints.MapControllers(); });
         }
