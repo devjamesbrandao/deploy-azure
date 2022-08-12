8e37d7514c65d91071d74aab8c0e161c665aea8f
James Noleto <muricijames@gmail.com>
2022-08-11 23:09:27 -0300

    feat: configuração e criação de método para obter token JWT

--- Tests/Integration/TodoControllerIntegrationTest.cs
+++ Tests/Integration/TodoControllerIntegrationTest.cs
@@ -1,21 +1,67 @@
 using System.Net;
+using FluentAssertions;
 using MeuTodo;
+using MeuTodo.ViewModels;
+using Microsoft.Extensions.Configuration;
+using Refit;
 using Tests.Factory;
+using Tests.HttpClients;
+using Xunit.Priority;
 
 namespace Tests.Integration
 {
+    [TestCaseOrderer(PriorityOrderer.Name, PriorityOrderer.Assembly)]
     public class TodoControllerIntegrationTest : IClassFixture<TestingWebAppFactory<Startup>>
     {
+        private readonly ILoginAPI _authTodo;
+        
         private readonly HttpClient _client;
 
-        public TodoControllerIntegrationTest(TestingWebAppFactory<Startup> factory)  => _client = factory.CreateClient();
+        private static string UserName { get; set; }
 
-        [Fact]
-        public async Task Uunauthorized_response_when_get_all_todos()
+        private static string Password { get; set; }
+
+        private static string TokenJWT { get; set; }
+
+        public TodoControllerIntegrationTest(TestingWebAppFactory<Startup> factory)  
         {
-            var response = await _client.GetAsync("v1/todos");
-            
-            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.Test.json")
+                .AddEnvironmentVariables();
+
+            var configuration = builder.Build();
+
+            _authTodo = RestService.For<ILoginAPI>(configuration.GetSection("UrlWebAppTestes").Value);
+
+            UserName = configuration.GetSection("APITodos_Access:Name").Value;
+
+            Password = configuration.GetSection("APITodos_Access:Password").Value;
+
+            // Artifice to add InMemoryDbContext
+            _client = factory.CreateClient();
+        }
+
+        [Fact, Priority(1)]
+        public async Task Get_JWT_Token_With_Success()
+        {
+            var token = await _authTodo.PostLogin(
+                new LoginUserViewModel()
+                {
+                    Name = UserName,
+                    Password = Password
+                }
+            );
+
+            TokenJWT = token.Token;
+
+            token.Token.Should().NotBeEmpty(because: "API require this JWT Token");
+        }
+
+        [Fact, Priority(2)]
+        public void Get_Unauthorized_wheh_Get_All_Todos()
+        {
+            Assert.True(true);
         }
     }
 }
