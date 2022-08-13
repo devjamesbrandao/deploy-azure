76b5c52ab199b350feffab4c897e18e466386630
James Noleto <muricijames@gmail.com>
2022-08-12 21:16:01 -0300

    feat: add descrição do viewModel para login

--- src/ViewModels/LoginUserViewModel.cs
+++ src/ViewModels/LoginUserViewModel.cs
@@ -4,8 +4,15 @@ namespace MeuTodo.ViewModels
 {
     public class LoginUserViewModel
     {
+        /// <summary>
+        /// Username
+        /// </summary>
         [Required]
         public string Name { get; set; }
+        /// <summary>
+        /// User password
+        /// </summary>
+        /// <value></value>
         [Required]
         public string Password { get; set; }
     }
