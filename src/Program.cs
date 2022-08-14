ad1b829a2b01e802b30f0d406b214a5f4fe96819
James Noleto <muricijames@gmail.com>
2022-08-14 13:12:32 -0300

    feat: add log com serilog

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,7 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace MeuTodo
 {
@@ -7,11 +9,30 @@ namespace MeuTodo
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .Enrich.FromLogContext()  
+            .WriteTo.Console()  
+            .CreateLogger();
+ 
+            try
+            {
+                Log.Information("Starting Web Host");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
